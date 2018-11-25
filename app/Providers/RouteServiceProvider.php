b9d46cca8c568a4aed47f1ccb3c0732e3c6bc296
vagrant <vagrant@homestead>
2018-11-25 02:42:32 -0300

    fixed hidden quests being accessible by id

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -12,76 +12,78 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Http\Controllers';
+	/**
+	 * This namespace is applied to your controller routes.
+	 *
+	 * In addition, it is set as the URL generator's root namespace.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
+	/**
+	 * Define your route model bindings, pattern filters, etc.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		parent::boot();
 
 		Route::model('questfilter', QuestFilter::class);
 		Route::model('shopitem', ShopItem::class);
 		Route::model('inventory', Inventory::class);
 		Route::bind('quest', function ($value) {
-			if(is_numeric($value)) {
-				return Quest::find($value);
+			if (is_numeric($value)) {
+				$quest = Quest::find($value);
+				if (!$quest->hidden) {
+					return $quest;
+				}
 			} else {
 				return Quest::where('code', $value)->first();
 			}
 		});
-    }
+	}
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapApiRoutes();
+	/**
+	 * Define the routes for the application.
+	 *
+	 * @return void
+	 */
+	public function map()
+	{
+		$this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+		$this->mapWebRoutes();
+		//
+	}
 
-        //
-    }
+	/**
+	 * Define the "web" routes for the application.
+	 *
+	 * These routes all receive session state, CSRF protection, etc.
+	 *
+	 * @return void
+	 */
+	protected function mapWebRoutes()
+	{
+		Route::middleware('web')
+			 ->namespace($this->namespace)
+			 ->group(base_path('routes/web.php'));
+	}
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
-    }
+	/**
+	 * Define the "api" routes for the application.
+	 *
+	 * These routes are typically stateless.
+	 *
+	 * @return void
+	 */
+	protected function mapApiRoutes()
+	{
+		Route::prefix('api')
+			 ->middleware('api')
+			 ->namespace($this->namespace)
+			 ->group(base_path('routes/api.php'));
+	}
 }
