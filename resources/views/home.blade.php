4f3852f442a8c381226a10357dc88203eaeab3be
Hugo Jeller <hugo_jeller@hotmail.com>
2018-12-21 16:44:38 -0300

    Updated homepage title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h1 class="display-4">Top quests</h1>
-            <p class="lead">Available quests now!</p>
+            <p class="lead">Random selected quests!</p>
         </div>
         @include('quests.card-deck', ['quests' => $randomQuests])
         
